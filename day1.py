009bf1331839450ef12f44ec13b08576e53930c9
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-02-17 23:48:24 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,4 +1,5 @@
  #  what is python ?
+ 
  ## It is a simple and easy to understand language , which feels like reading English only .
  # this pseudo code nature of python makes it easy to understand .
  
